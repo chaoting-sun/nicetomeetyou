b08e96e64d744ad506b6589e3517c2aa6bb8815a
chaoting-sun <chaoting0108@gmail.com>
2026-03-22 15:58:49 +0800

    feat: add logging configuration to surface INFO-level scraper output

--- config/settings.py
+++ config/settings.py
@@ -111,3 +111,24 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": 3600,
     },
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {name} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}

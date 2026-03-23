6bc5f0112c0b2ae145f2fd0efafa470c4145a2db
chaoting-sun <chaoting0108@gmail.com>
2026-03-23 18:32:54 +0800

    feat: add news scraping step to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ if [ "$1" = "gunicorn" ]; then
 
     echo "Collecting static files..."
     python manage.py collectstatic --noinput
+
+    echo "Running news scraper..."
+    python manage.py scrape_news
 fi
 
 exec "$@"
